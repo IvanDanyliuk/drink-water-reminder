7b0252a5b007dd7a07a4e9aa65343ee8498ada4c
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-06-17 14:47:39 +0300

    implement the My Banks page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ 
     userId: loggedIn.$id 
-  })
+  });
 
   if(!accounts) return;
   
